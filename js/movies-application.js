53e84543f4e0845492e37ed9858f829bf7bed5cb
Michaela <michaela.arteberry@gmail.com>
2023-12-07 17:11:54 -0500

    completeley redid the js, still working the kinks

--- js/movies-application.js
+++ js/movies-application.js
@@ -1,5 +1,4 @@
 "use strict";
-
 const options = {
     method: 'GET',
     headers: {
@@ -7,41 +6,47 @@ const options = {
         Authorization: AUTH_KEY
     }
 };
-fetch('https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
-fetch("http://localhost:3000/movies").then(resp => resp.json()).then(data => console.log(data));
-function searchMovies(e) {
-    e.preventDefault()
-    const url = 'https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1&query=';
-    // Clear the previous movie cards
-    const movieContainer = document.getElementById('movies');
-    movieContainer.innerHTML = '';
-    // Get the user's search input
-    let searchText = document.getElementById('searchBar').value;
-    // Fetch the movies from the TMDb API
-    fetch(url + searchText, options)
+function saveToLocalStorage(key, data) {
+    localStorage.setItem(key, JSON.stringify(data));
+}
+function getFromLocalStorage(key) {
+    const data = localStorage.getItem(key);
+    return data ? JSON.parse(data) : null;
+}
+// Function to fetch movies from TMDb API
+function fetchMovies(searchText) {
+    const url = `https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1&query=${searchText}`;
+    return fetch(url, options)
         .then(response => response.json())
         .then(data => {
-            // Loop through the results and create a movie card for each one
-            data.results[0].forEach(movie => {
-                let movieCard = `
-                    <div>
-                        <h3>${movie.title}</h3>
-                        <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}"/>
-                        <p>${movie.overview}</p>
-                    </div>
-                `;
-                // Append the movie card to the container
-                movieContainer.innerHTML += movieCard;
-            });
-        })
+            saveToLocalStorage('searchResults', data.results);
+            return data.results;
+        });
+}
+// Function to display only the first movie on the page
+function displayMovie(result) {
+    const movieContainer = document.getElementById('movies');
+    movieContainer.innerHTML = '';
+    if (result.length > 0) {
+        const movie = result[0];
+        let movieCard = `
+            <div>
+                <h3>${movie.title}</h3>
+                <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}"/>
+                <p>${movie.overview}</p>
+            </div>
+        `;
+        movieContainer.innerHTML += movieCard;
+    } else {
+        movieContainer.innerHTML = '<p>No results found</p>';
+    }
+}
+// Function to handle the search
+function searchMovies(e) {
+    e.preventDefault();
+    const searchText = document.getElementById('searchBar').value;
+    fetchMovies(searchText)
+        .then(displayMovie)
         .catch(error => console.error('Error:', error));
 }
 document.getElementById("submitSearch").addEventListener("click", searchMovies);
-
-
-
-
-
