828795e96e719e4d60783c801c789a8c189ae44c
Seth Crist <sethcrist50@gmail.com>
2023-12-07 15:05:22 -0600

    made corrections

--- js/movies-application.js
+++ js/movies-application.js
@@ -1,6 +1,4 @@
 "use strict";
-// const MOVIE_KEY = "7f4f98e5614adf52b2fdfeeb75a48c97"
-
 
 const options = {
     method: 'GET',
@@ -13,118 +11,37 @@ fetch('https://api.themoviedb.org/3/search/movie?include_adult=false&language=en
     .then(response => response.json())
     .then(response => console.log(response))
     .catch(err => console.error(err));
-
-//Object array in the console log "shrek
-    //fetch('https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1').then(resp => resp.json()).then(data =>
-        fetch("http://localhost:3000/movies").then(resp => resp.json()).then(data => console.log(data));
-
-    const createMovie = async (movie) => {
-        try {
-            const url = `http://localhost:3000/movies`;
-            const options = {
-                method: "POST",
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(movie)
-            };
-            const resp = await fetch(url, options);
-            const newMovie = await resp.json();
-            return newMovie;
-        } catch (error) {
-            console.error(error);
-        }
-    }
-
-
-    const editMovie = async (id, movie) => {
-        try {
-            const url = `http://localhost:3000/movie/${id}`;
-            const options = {
-                method: "PATCH",
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(movie)
-            };
-            const resp = await fetch(url, options);
-            const newMovie = await resp.json();
-            return newMovie;
-        } catch (error) {
-            console.error(error);
-        }
-    }
-
-    // editBook(5, {"authorId": 5});
-
-    // function populateDropDown() {
-    //     fetch("http://localhost:3000/books").then(resp => resp.json()).then(data => {
-    //         console.log(data);
-    //         const dropDown = document.getElementById("edit-select");
-    //         for (let book of data) {
-    //             const option = document.createElement("option");
-    //             option.value = book.id;
-    //             option.innerText = book.title;
-    //             dropDown.appendChild(option);
-    //         }
-    //     })
-    // }
-
-    // populateDropDown();
-    //
-    // document.querySelector("#edit-select").addEventListener("change", (e) => {
-    //     const bookId = e.target.value;
-    //     fetch("http://localhost:3000/books/" + bookId).then(resp => resp.json()).then(book => {
-    //         document.querySelector("#edit-title").value = book.title;
-    //         document.querySelector("#edit-isbn").value = book.ISBN;
-    //         document.querySelector("#edit-genre").value = book.genre;
-    //         document.querySelector("#edit-year").value = book.publishedYear;
-    //         document.querySelector("#edit-summary").value = book.summary;
-    //     })
-    // });
-
-    // document.forms.editForm.addEventListener("submit", e => {
-    //     e.preventDefault();
-    //     let id = document.querySelector("#edit-select").value;
-    //     let title = document.querySelector("#edit-title").value;
-    //     let ISBN = document.querySelector("#edit-isbn").value;
-    //     let genre = document.querySelector("#edit-genre").value;
-    //     let publishedYear = document.querySelector("#edit-year").value;
-    //     let summary = document.querySelector("#edit-summary").value;
-    //     editBook(id, {title, ISBN, genre, publishedYear, summary});
-    //     populateDropDown();
-    // });
-    //
-    // fetch("http://localhost:3000/books", {method: "DELETE"});
-
-    // createAuthor(jimDavis);
-
-    // createBook(newBook).then(() => fetch("http://localhost:3000/books")).then(resp => resp.json()).then(data => console.log(data));
+fetch("http://localhost:3000/movies").then(resp => resp.json()).then(data => console.log(data));
+function searchMovies(e) {
+    e.preventDefault()
+    const url = 'https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1&query=';
+    // Clear the previous movie cards
+    const movieContainer = document.getElementById('movies');
+    movieContainer.innerHTML = '';
+    // Get the user's search input
+    let searchText = document.getElementById('searchBar').value;
+    // Fetch the movies from the TMDb API
+    fetch(url + searchText, options)
+        .then(response => response.json())
+        .then(data => {
+            // Loop through the results and create a movie card for each one
+            data.results[0].forEach(movie => {
+                let movieCard = `
+                    <div>
+                        <h3>${movie.title}</h3>
+                        <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}"/>
+                        <p>${movie.overview}</p>
+                    </div>
+                `;
+                // Append the movie card to the container
+                movieContainer.innerHTML += movieCard;
+            });
+        })
+        .catch(error => console.error('Error:', error));
+}
+document.getElementById("submitSearch").addEventListener("click", searchMovies);
 
 
-    // On Load up adds movie cards to page
-    async function renderMovieCard(movie) {
-        const response = await fetch(`http://localhost:3000/movie/1`);
-        const movieData = await response.json();
-        const movieContainer = document.getElementById("movies");
-        // const MOVIE_KEY = "3d3318fc";
-
-// Create HTML structure for the movie card
-
-                let movieHTML = document.createElement("div");
-                    movieHTML.innerHTML += ` 
-                        <img src="${movieData.Poster}" alt="${movieData.Title} Poster"/>
-                        <h3>${movieData.Title}</h3>
-                        <p>${movieData.imdbRating}</p>
-                        <p>${movieData.Plot}</p>
-                        
-`;
-        movieContainer.appendChild(movieHTML);
-        document.getElementById('movies').innerHTML = movieHTML;
-                };
 
 
-// <img src="http://img.omdbapi.com/?apikey=[3d3318fc]&"/>
-// <h3>${movie.title}</h3>
-// <p>${movie.rating}</p>
-// <p>${movie.plot}</p>
+
